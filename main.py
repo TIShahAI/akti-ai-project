333453dedd3228dd24a52a3320f4f96c550644e3
tahir_iqbal_shah <tahiriqbal23456@gmail.com>
2025-01-12 14:56:04 -0800

    changed url

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
-    docs_url="/",
     title="Chatbot API",
     description="A simple chatbot API",
     version="0.1"
