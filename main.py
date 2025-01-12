741327b5b7ed1003f14e561994b34430de11db5d
tahir_iqbal_shah <tahiriqbal23456@gmail.com>
2025-01-12 14:53:48 -0800

    created API

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from chatbot import chat
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
-print(chat("what is aldehyde"))
+app = FastAPI(
+    docs_url="/",
+    title="Chatbot API",
+    description="A simple chatbot API",
+    version="0.1"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/ask")
+def handle_question(question:str):
+    response = chat(question)
+    return JSONResponse(content={"response": response})
