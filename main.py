d6ff9d5de42a54e2bc61bf2836d01753645f9de8
tahir_iqbal_shah <tahiriqbal23456@gmail.com>
2025-02-09 12:09:34 +0500

    init

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from chatbot import chat
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI,Request
+from fastapi.responses import JSONResponse,HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
 
 app = FastAPI(
-    docs_url="/",
     title="Chatbot API",
     description="A simple chatbot API",
     version="0.1"
@@ -17,6 +19,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+templates = Jinja2Templates(directory="templates")
+
+# Serve the chatbot HTML page
+@app.get("/", response_class=HTMLResponse)
+async def get_chatbot_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/ask")
 def handle_question(question:str):
--- prompt.py
+++ prompt.py
@@ -1,6 +1,15 @@
 PROMPT = """
 you are a chemistry teacher , 
 whenever you are asked any question , 
-you shall give answer not more than 50 words 
-
-"""
+you shall give answer from the prescribed pdf & not more than 50 words .
+If you are asked to prepare MCQ, you must follow the folowing steps
+ 1. Make a loggical MCQ with four options, options shall be indicated with small alphabets having parentheses. 
+ 2. One of the option shall be correct and all rest three shall be wrong, and all options shall be from the prescribed pdf .
+ 3. Highlight the correct answer with Yellow colour.
+ 4. then ask for the anwser from the user and tell him to write the alphabet indicating the correct answer.
+ 5. match the alphabet given by the user with the alphabet of the correct answer,  If it matches , high light the user's alphabet Green otherwise Red.
+ 6. if system response is Red highlight, then highlight then answer with l.blue in the system's question.
+ 7. if system response is Green, then ask the user "Due you want to go through another MCQ ? (Y/N)"
+ 8. if user response is "Y" go to step 1, else "Thank you".
+ 9. 
+""" 
